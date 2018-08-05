58108fc721302f1bb07a1d42f60537675c86116a
Eric <dev@derewonko.com>
2018-08-05 12:15:22 -0400

    disable useless http cache

--- tuScraper/settings.py
+++ tuScraper/settings.py
@@ -87,7 +87,7 @@ COOKIES_ENABLED = False
 
 # Enable and configure HTTP caching (disabled by default)
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-HTTPCACHE_ENABLED = True          # Default: False
+HTTPCACHE_ENABLED = False          # Default: False
 HTTPCACHE_EXPIRATION_SECS = 0     # Default: 0
 HTTPCACHE_DIR = 'httpcache'
 HTTPCACHE_IGNORE_HTTP_CODES = []  # Don’t cache response with these HTTP codes
